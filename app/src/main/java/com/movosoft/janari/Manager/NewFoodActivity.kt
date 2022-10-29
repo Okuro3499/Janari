1bf7029d1f0c186623ecca4f1dbbb23112120e71
koleladan <isaiahodan@gmail.com>
2022-10-28 21:38:26 -0700

    update

---
+++ app/src/main/java/com/movosoft/janari/Manager/NewFoodActivity.kt
@@ -0,0 +1,11 @@
+package com.movosoft.janari.Manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_food)
+    }
+}
